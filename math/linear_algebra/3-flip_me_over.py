686779f1c97fdfe4d7f0df4b48ae1b3d6758d906
Quentin <48597772+loufi84@users.noreply.github.com>
2025-12-02 07:38:37 +0100

    First matrix transpose in Python

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that returns the transpose of a 2D matrix.
+"""
+
+
+def matrix_transpose(matrix):
+    """
+    The function that returns the transpose of a 2D matrix.
+    """
+    new_matrix = [[matrix[i][j] for i in range(len(matrix))] for j in range(len(matrix[0]))]
+    return new_matrix
