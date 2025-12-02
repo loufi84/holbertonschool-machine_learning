688097e967195182bd356209c34a046e2fb50fed
Quentin <48597772+loufi84@users.noreply.github.com>
2025-12-02 16:40:36 +0100

    Added a function to concatenates two matrices along an axis

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
-import numpy as np
 """
 A module that contains a function that concatenates two matrices along
 a specific axis.
-
 """
+import numpy as np
+
 
 
 def np_cat(mat1, mat2, axis=0):
     """
     The function that concatenates two matrices along a
     specified axis.
-
     """
     new_matrix = np.concatenate((mat1, mat2), axis)
     return new_matrix
