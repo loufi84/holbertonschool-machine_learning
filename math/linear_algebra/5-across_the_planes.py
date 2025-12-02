24da917ef9004e599b5c36271647da9126a2eb43
Quentin <48597772+loufi84@users.noreply.github.com>
2025-12-02 10:29:38 +0100

    Added a function two add two 2D matrices element-wise

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -10,10 +10,11 @@ def add_matrices2D(mat1, mat2):
     The function to add two matrices element-wise.
     """
     shape = __import__('2-size_me_please').matrix_shape
-    if shape(mat1) != shape(mat2):
-        return None
     if not mat1 or not mat2:
         return None
+    if shape(mat1) != shape(mat2):
+        return None
     new_matrix = []
-    new_matrix = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
+    new_matrix = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))]
+                  for i in range(len(mat1))]
     return new_matrix
