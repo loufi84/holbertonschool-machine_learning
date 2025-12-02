e51d361b12cf090c46157b8fbeaf1cd58db0de03
Quentin <48597772+loufi84@users.noreply.github.com>
2025-12-02 15:27:30 +0100

    Added a transpose function using numpy

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import numpy as np
+"""
+A module that contains a single function that uses NumPy to transposes
+a matrix.
+"""
+
+
+def np_transpose(matrix):
+    """
+    The function to transposes the matrix using NumPy.
+    """
+    new_matrix = matrix.T
+    return new_matrix
