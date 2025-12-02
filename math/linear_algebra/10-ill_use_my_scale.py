53c2ef222ecf4bccd3fb75b88d83e4aeb21e1a16
Quentin <48597772+loufi84@users.noreply.github.com>
2025-12-02 15:19:58 +0100

    Added a numpy function that calculates the shape of an array

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+A module that contains a function to calculate the shape of an array
+using NumPy.
+"""
+
+
+def np_shape(matrix):
+    """
+    The function to calculate the shape of an array using NumPy.
+    """
+    return matrix.shape
