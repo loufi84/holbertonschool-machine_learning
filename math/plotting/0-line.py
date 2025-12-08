bbe32179d083036a7cc9e284edf4d4b72b126a9f
Quentin <48597772+loufi84@users.noreply.github.com>
2025-12-08 11:07:42 +0100

    Adding pycodestyle + documentation

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
+"""
+This module provides a function that draw a line graph.
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-def line():
 
+def line():
+    """
+    The function to draw the graph.
+    """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
