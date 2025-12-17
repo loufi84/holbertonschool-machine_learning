75652a2740d42279618f40e1361e0c4bb96b08da
Quentin <48597772+loufi84@users.noreply.github.com>
2025-12-17 15:05:25 +0100

    Task 7

--- supervised_learning/decision_tree/7-build_decision_tree.py
+++ supervised_learning/decision_tree/7-build_decision_tree.py
@@ -335,11 +335,11 @@ class Decision_Tree():
 
         if verbose == 1:
             print(f"""  Training finished.
-- Depth                     : {self.depth()}
-- Number of nodes           : {self.count_nodes()}
-- Number of leaves          : {self.count_nodes(only_leaves=True)}
-- Accuracy on training data : {self.accuracy(self.explanatory,
-                                             self.target)}""")
+            - Depth                     : {self.depth()}
+            - Number of nodes           : {self.count_nodes()}
+            - Number of leaves          : {self.count_nodes(only_leaves=True)}
+            - Accuracy on training data : {self.accuracy(self.explanatory,
+                                                         self.target)}""")
 
     def fit_node(self, node):
         """
