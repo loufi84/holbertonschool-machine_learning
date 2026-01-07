823248bc09dc27619585e0df98ef471591dc17a9
Quentin <48597772+loufi84@users.noreply.github.com>
2026-01-07 14:21:43 +0100

    Task 2

---
+++ supervised_learning/classification/2-neuron.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+This module contains the definition of the class Neuron
+"""
+import numpy as np
+
+
+class Neuron:
+    """
+    The definition of the class Neuron
+    """
+    def __init__(self, nx):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """
+        Getter for __W
+        """
+        return self.__W
+
+    @property
+    def b(self):
+        """
+        Getter for __b
+        """
+        return self.__b
+
+    @property
+    def A(self):
+        """
+        Getter for __A
+        """
+        return self.__A
+
+    def forward_prop(self, X):
+        """
+        A function that calculate the forward propagation.
+        """
+        Z = np.dot(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
