e03a197aaf6362f8cd8210bd1a9dec1a3b57f357
Quentin <48597772+loufi84@users.noreply.github.com>
2026-01-07 14:39:40 +0100

    Task 5

---
+++ supervised_learning/classification/5-neuron.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+"""
+This module contains the definition of the class Neuron
+"""
+import numpy as np
+
+
+class Neuron:
+    """
+    The definition of the class Neuron
+    """
+    def __init__(self, nx):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """
+        Getter for __W
+        """
+        return self.__W
+
+    @property
+    def b(self):
+        """
+        Getter for __b
+        """
+        return self.__b
+
+    @property
+    def A(self):
+        """
+        Getter for __A
+        """
+        return self.__A
+
+    def forward_prop(self, X):
+        """
+        A function that calculate the forward propagation.
+        """
+        Z = np.dot(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """
+        Calculates the cost of the model using logistic regression.
+        """
+        m = Y.shape[1]
+        cost = - (1 / m) * np.sum(
+            Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        )
+        return cost
+
+    def evaluate(self, X, Y):
+        """
+        A function that evaluates the prediction of the neuron.
+        """
+        A = self.forward_prop(X)
+        cost = self.cost(Y, A)
+
+        prediction = np.where(A >= 0.5, 1, 0)
+        return prediction, cost
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """
+        Calculate one pass of gradient descent on the neuron.
+        """
+        m = Y.shape[1]
+        dZ = A - Y
+
+        dW = (1 / m) * np.dot(dZ, X.T)
+        db = (1 / m) * np.sum(dZ)
+
+        self.__W = self.__W - (alpha * dW)
+        self.__b = self.__b - (alpha * db)
