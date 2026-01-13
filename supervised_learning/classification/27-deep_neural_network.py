35f28b3865bf1c5269cb9ce9d6a74f12574502b4
Quentin <48597772+loufi84@users.noreply.github.com>
2026-01-13 12:02:36 +0100

    Task 27

--- supervised_learning/classification/27-deep_neural_network.py
+++ supervised_learning/classification/27-deep_neural_network.py
@@ -87,7 +87,7 @@ class DeepNeuralNetwork:
         regression.
         """
         m = Y.shape[1]
-        cost = -(1 / m) * np.sum(Y * np.log(A + 1e-8))
+        cost = -(1 / m) * np.sum(Y * np.log(A + 1.0000001))
         return cost
 
     def evaluate(self, X, Y):
