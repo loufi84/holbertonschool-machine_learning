2fceb1037eee767ef536f834eb3afab99d4a54a0
Quentin <48597772+loufi84@users.noreply.github.com>
2026-01-13 10:05:57 +0100

    Task 16

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -15,7 +15,6 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
 
@@ -23,16 +22,13 @@ class DeepNeuralNetwork:
         self.cache = {}
         self.weights = {}
 
-        for l in range(1, self.L + 1):
-            layer_size = layers[l - 1]
+        prev_size = nx
+        for l, layer_size in enumerate(layers, start=1):
+            if not isinstance(layer_size, int) or layer_size <= 0:
+                raise TypeError("layers must be a list of positive integers")
 
-            if l == 1:
-                prev_size = nx
-            else:
-                prev_size = layers[l - 2]
-
-            self.weights[f"W{1}"] = (
-                np.random.randn(layer_size, prev_size)
-                * np.sqrt(2 / prev_size)
+            self.weights[f"W{l}"] = (
+                np.random.randn(layer_size, prev_size) * np.sqrt(2 / prev_size)
             )
-            self.weights[f"b{1}"] = np.zeros((layer_size, 1))
+            self.weights[f"b{l}"] = np.zeros((layer_size, 1))
+            prev_size = layer_size
