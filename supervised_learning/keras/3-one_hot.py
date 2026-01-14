06954a556554f33929f309cf2f23b12f95197cd6
loufi84 <quentin.lataste@gmail.com>
2026-01-14 10:58:52 +0100

    Task 3

---
+++ supervised_learning/keras/3-one_hot.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module contains the function one_hot using Keras library.
+"""
+import tensorflow.keras as K
+
+
+def one_hot(labels, classes=None):
+    """
+    The function to one_hot.
+    """
+    return K.utils.to_categorical(labels, num-classes=classes)
