140120e32a6e8bba6738cfbab566e9fd6acd87b4
loufi84 <quentin.lataste@gmail.com>
2026-01-15 16:07:46 +0100

    Task 5

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that trains a neural network
+with Keras.
+"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """
+    The function to train the model.
+    """
+    history = network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle,
+        validation_data=validation_data
+    )
+
+    return history
