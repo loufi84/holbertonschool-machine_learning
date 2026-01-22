ef5aef0f6572bedcaf19eaa8c1be7caf2b8e51dc
loufi84 <quentin.lataste@gmail.com>
2026-01-22 17:30:14 +0100

    Task 11

---
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that updates the learning rate using
+inverse time decay.
+"""
+import numpy as np
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """
+    Updates the learning rate using inverse time decay (stepwise).
+    """
+    return alpha / (1 + decay_rate * (global_step // decay_step))
