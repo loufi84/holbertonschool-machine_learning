44393c3f0e67889918a2b3f322a41ec02d95921e
loufi84 <quentin.lataste@gmail.com>
2026-01-22 17:18:48 +0100

    Task 5

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that updates a variable using the gradient
+descent with momentum optimization.
+"""
+import numpy as np
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """
+    Updates a variable using gradient descent with momentum.
+    """
+    v = beta1 * v + (1 - beta1) * grad
+    var = var - alpha * v
+
+    return var, v
